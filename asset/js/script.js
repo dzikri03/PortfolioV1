57ae2d089c77f42e2944bb800353cebc0e62d9d8
nrtflix <dzikridmy@gmail.com>
2023-05-23 13:05:23 +0700

    lolo

---
+++ asset/js/script.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Web Developer", "Mobile Developer", "Web Design"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
